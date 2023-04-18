9c1ff364e6c7a7aac29e03501e8c41974c8a8927
J <jjout14@gmail.com>
2023-04-18 13:04:08 -0400

    The initial work on the portfolio site

---
+++ src/Components/Hero.js
@@ -0,0 +1,15 @@
+
+export default function Hero() {
+    return (
+        <div>
+            <div>
+                <h1 className="Name">Jeremy Johnson</h1>
+                <h3>Front End Developer</h3>
+            </div>
+            <div className='Hero-nav-buttons'>
+                {/*will create large button components*/}
+                About, Project, Contact
+            </div>
+        </div>
+    )
+}
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+export default function NavBar() {
+    return (
+        <div>
+            <div>
+                <ul className="nav-items">
+                    {/* create a tags for each item that links to different parts of the page*/}
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
